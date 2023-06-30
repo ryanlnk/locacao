ef9b9f965f6fb90b260b524b79b5bab281b158b5
Ryan Silva <rdsp1@aluno.ifnmg.edu.br>
2023-06-29 21:24:49 -0300

    Instalando o breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Admin\Carros\Carros;
 use App\Http\Livewire\Admin\Carros\CarrosForm;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
 Route::get('/', Carros::class);
 
 Route::get('/carros', Carros::class);
@@ -22,3 +28,16 @@ Route::get('/carros', Carros::class);
 Route::get('/carroscriar', CarrosForm::class);
 
 Route::get('/carros/{carro}/editar', CarrosForm::class);
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
